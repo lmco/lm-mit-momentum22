4f230f783a8b0f94216532e812436a959eb6e6b1
Irina Lavryonova <katabeta@users.noreply.github.com>
2021-11-09 14:56:42 -0500

    Update starting location to KBDR

--- launch_px4_stratford.bash
+++ launch_px4_stratford.bash
@@ -4,8 +4,8 @@
 ##################################################################
 
 cd ~/Momentum/PX4/PX4-Autopilot/
-export PX4_HOME_LAT=41.15
-export PX4_HOME_LON=-75.14
+export PX4_HOME_LAT=41.167039589529786
+export PX4_HOME_LON=-73.12708909898214
 HEADLESS=1 make px4_sitl jmavsim
 sleep 5
 param set MPC_Z_VEL_MAX_DN 1.0
