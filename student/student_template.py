6197e09f568e837db67e29ba432968d59198419f
Irina Lavryonova <katabeta@users.noreply.github.com>
2021-11-09 12:00:42 -0500

    Create student_template.py

---
+++ student/student_template.py
@@ -0,0 +1,19 @@
+from student_base import student_base
+import time
+import numpy
+
+class my_flight_controller(student_base):
+	
+	def student_run(self, telemetry, commands):
+		# Student code goes in this method.
+    # See student_example.py for ideas on
+    # how to fill out this method to complete the challenge.
+    pass
+		
+		
+# This bit of code just makes it so that this class actually runs when executed from the command line,
+# rather than just being silently defined.
+
+if __name__ == "__main__":
+	fcs = my_flight_controller()
+	fcs.run()
