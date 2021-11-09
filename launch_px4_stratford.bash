1339b561835bc4a4f6053d7c907789b4c9274488
Irina Lavryonova <katabeta@users.noreply.github.com>
2021-11-09 14:55:34 -0500

    Change the sim environment target to jmavsim

--- launch_px4_stratford.bash
+++ launch_px4_stratford.bash
@@ -6,7 +6,7 @@
 cd ~/Momentum/PX4/PX4-Autopilot/
 export PX4_HOME_LAT=41.15
 export PX4_HOME_LON=-75.14
-HEADLESS=1 make px4_sitl gazebo
+HEADLESS=1 make px4_sitl jmavsim
 sleep 5
 param set MPC_Z_VEL_MAX_DN 1.0
 param set MPC_Z_VEL_MAX_UP 3.0
