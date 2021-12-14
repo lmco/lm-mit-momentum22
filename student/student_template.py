3bd364fb0d94d1fe6587741116264b2c62d19497
Irina Lavryonova <lavryonovairina@gmail.com>
2021-12-13 20:38:04 -0500

    Fix indenting issue in student template

--- student/student_template.py
+++ student/student_template.py
@@ -6,9 +6,9 @@ class my_flight_controller(student_base):
 	
 	def student_run(self, telemetry, commands):
 		# Student code goes in this method.
-    # See student_example.py for ideas on
-    # how to fill out this method to complete the challenge.
-    pass
+		# See student_example.py for ideas on
+		# how to fill out this method to complete the challenge.
+		pass
 		
 		
 # This bit of code just makes it so that this class actually runs when executed from the command line,
