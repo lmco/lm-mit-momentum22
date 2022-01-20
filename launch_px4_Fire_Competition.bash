75f5b2d686de5b0079f1f68e1358515963688fca
Irina Lavryonova <lavryonovairina@gmail.com>
2022-01-20 01:01:51 -0500

    Set sim speed to real time in bash scripts

--- launch_px4_Fire_Competition.bash
+++ launch_px4_Fire_Competition.bash
@@ -7,6 +7,7 @@ cd ~/Momentum/PX4/PX4-Autopilot/
 export PX4_HOME_LAT=42.98575
 export PX4_HOME_LON=-70.6185
 export PX4_HOME_ALT=10
+export PX4_SIM_SPEED_FACTOR=1
 HEADLESS=1 make px4_sitl jmavsim
 sleep 5
 param set MPC_Z_VEL_MAX_DN 1.0
